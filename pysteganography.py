992e30c083de07e2f23a364099709be74866a463
James Lee <jamesl33info@gmail.com>
2018-01-27 00:55:36 +0000

    Can now encode loads more file types
    Changed the way files are encoded so that loads more file
    types can be encoded. This in return requires the installation of
    the 'bitstring' module
    
    Made the encoding and decoding much faster by removing unnecessary
    conversions and additional 'len' function calls.
    
    Separated command line interface and steganography module. This
    will allow easier integration into the GUI.

--- pysteganography.py
+++ pysteganography.py
@@ -1,188 +1,137 @@
 #!/usr/bin/python3
-"""pysteganography: Python script for the management of steganographic images
+""" Author: James Lee
+Email: jamesl33info@gmail.com
+Supported Python version: 3.5.2+
 
-References:
-text_to_bits - https://stackoverflow.com/questions/7396849/convert-binary-to-ascii-and-vice-versa
-text_from_bits - https://stackoverflow.com/questions/7396849/convert-binary-to-ascii-and-vice-versa
+Python script to allow the manipulation of steganographic images
 """
 
+import sys
+
+import bitstring
 from PIL import Image
 
+
 class Steganography():
-    """Steganography: Class which allows the creation and reading of steganographic images
+    """ Create and reads steganographic images made using last bit steganography
 
-    :param image_path: image which will be read using 'python-pillow'
-    :param save_path: path to where output images will be saved
+    Keyword arguments:
+    image_path -- The location of the image which this class will manipulate
     """
-    def __init__(self, image_path, save_path=None):
-        if save_path is None:
-            self.save_path = 'steg-'
-            self.save_path += image_path
-        else:
-            self.save_path = save_path
 
-        self.image = Image.open(image_path)
-        self.image = self.image.convert('RGBA')
+    def __init__(self, image_path):
+        try:
+            self.image = Image.open(image_path)
+        except FileNotFoundError:
+            print('File not found')
+            sys.exit(1)
+
         self.pixels = self.image.load()
+        self.image_path = image_path
+        self.width = self.image.size[0]
+        self.height = self.image.size[1]
+        self.channels = len(self.pixels[0, 0])
 
-    def encode_text(self, message):
-        """encode_text: Encode 'message' into a 'png' or 'bmp' image
+    def encode_file(self, file_path):
+        """ Encode a file into a 'png' or 'bmp' image
 
-        :param message: string to be hidden in the image
+        Keyword arguments:
+        file_path -- Path to file which will be hidden in the image
         """
-        bit = 0
-        message = self.text_to_bits(message)
-        size = '{0:032b}'.format(len(message))
-        message = size + message
+        data = bitstring.ConstBitStream(filename=file_path)
+        data = data.read('bin')
+        self._encode_bitstring(data)
 
-        if len(message) > self.image.size[0] * self.image.size[1] * 4:
+    def decode_file(self):
+        """ Decode a file hidden in a 'png' or 'bmp' image """
+        data = self._decode_bitstring()
+
+        # TODO Make output filename match name of original file
+        with open('steg-output', 'wb+') as output_file:
+            data.tofile(output_file)
+
+    def _encode_bitstring(self, bstring):
+        """ Encode a 'bitstring' object into a 'png' or 'bmp' image
+
+        Keyword arguments:
+        bstring -- 'bitstring' object which will be hidden the image
+        """
+        index = 0
+        size = '{0:032b}'.format(len(bstring))
+        bstring = size + bstring
+        bstring_length = len(bstring)
+
+        if bstring_length > self.width * self.height * self.channels:
             raise ValueError('No enough room in image to store this message')
 
-        for row in range(self.image.size[0]):
-            for col in range(self.image.size[1]):
-                for cha in range(3):
-                    if bit >= len(message):
+        for row in range(self.width):
+            for col in range(self.height):
+                for cha in range(self.channels):
+                    try:
+                        new_pixel_val = list(self.pixels[row, col])
+                        new_pixel_val[cha] = self._set_lsb(new_pixel_val[cha], int(bstring[index]))
+                        self.pixels[row, col] = tuple(new_pixel_val)
+                        index += 1
+                    except IndexError:
                         break
-                    if cha == 0:
-                        self.pixels[row, col] = (self.set_lsb(self.pixels[row, col][0], int(message[bit])),
-                                                 self.pixels[row, col][1],
-                                                 self.pixels[row, col][2],
-                                                 self.pixels[row, col][3])
-                    elif cha == 1:
-                        self.pixels[row, col] = (self.pixels[row, col][0],
-                                                 self.set_lsb(self.pixels[row, col][1], int(message[bit])),
-                                                 self.pixels[row, col][2],
-                                                 self.pixels[row, col][3])
-                    elif cha == 2:
-                        self.pixels[row, col] = (self.pixels[row, col][0],
-                                                 self.pixels[row, col][1],
-                                                 self.set_lsb(self.pixels[row, col][2], int(message[bit])),
-                                                 self.pixels[row, col][3])
-                    elif cha == 3:
-                        self.pixels[row, col] = (self.pixels[row, col][0],
-                                                 self.pixels[row, col][1],
-                                                 self.pixels[row, col][2],
-                                                 self.set_lsb(self.pixels[row, col][3], int(message[bit])))
-                    bit += 1
 
-        self.image.save(self.save_path)
+        self.image.save('steg-' + self.image_path)
 
-    def decode_text(self):
-        """decode_text: Attempt to read an image for a hidden message
+    def _decode_bitstring(self):
+        """ Decode a 'bitstring' object from a 'png' or 'bmp' image
+
+        Returns:
+        bitstring -- binary representation of decoded file
         """
-        message = ''
-        message_len = ''
-        for row in range(self.image.size[0]):
-            for col in range(self.image.size[1]):
-                for cha in range(3):
-                    if len(message_len) == 32:
-                        break
-                    message_len += str(self.get_lsb(self.pixels[row, col][cha]))
 
-        message_len = ((int(message_len, 2)) + 32)
+        def image_loop(end):
+            """ Loop through image pixels to obtain 'bitstring' object
 
-        for row in range(self.image.size[0]):
-            for col in range(self.image.size[1]):
-                for cha in range(3):
-                    if len(message) == message_len:
-                        break
-                    message += str(self.get_lsb(self.pixels[row, col][cha]))
+            Keyword arguments:
+            end -- How many bits to decode
 
-        message = message[32:]
-        return self.text_from_bits(message)
+            Returns:
+            str -- binary
+            """
+            data = ''
+            current_length = 0
+            for row in range(self.width):
+                for col in range(self.height):
+                    for cha in range(self.channels):
+                        if current_length == end:
+                            break
+                        data += str(self._get_lsb(self.pixels[row, col][cha]))
+                        current_length += 1
+            return data
+
+        bstring_length = ((int(image_loop(32), 2)) + 32)
+        bstring = image_loop(bstring_length)
+
+        return bitstring.Bits('0b' + bstring[32:])
 
     @classmethod
-    def get_lsb(cls, num):
-        """get_lsb: Get the least significant bit of input number
+    def _get_lsb(cls, number):
+        """ Get the least significant bit on an integer
 
-        :param num: Number to get the lsb of
+        Keyword arguments:
+        number -- Integer value to fetch the least significant bit of
+
+        Returns:
+        Int -- Least significant bit on input iteger
         """
-        return num & 1
+        return number & 1
 
     @classmethod
-    def set_lsb(cls, number, bit_value):
-        """set_lsb: Set the least significant bit of a number *Does not actually modify the number*
+    def _set_lsb(cls, number, bit_value):
+        """ Modifys and integer and sets the least significant bit
 
-        :param number: Number which to set the lsb
-        :param bit_value: Boolean value of what to set the lsb too
+        Keyword arguments:
+        number -- Integer value to set the least significant bit of
+        bit_value -- Boolean value depending if you want to set the bit to '1' or '0'
+
+        Returns:
+        Int -- Value with least significant bit set
         """
         if bit_value:
             return number | bit_value
         return (number & ~1) | bit_value
-
-    @classmethod
-    def text_to_bits(cls, text, encoding='utf-8', errors='surrogatepass'):
-        """text_to_bits:
-        Convert text to binary
-
-        :param text: Text to be converted
-        """
-        bits = bin(int.from_bytes(text.encode(encoding, errors), 'big'))[2:]
-        return bits.zfill(8 * ((len(bits) + 7) // 8))
-
-    @classmethod
-    def text_from_bits(cls, bits, encoding='utf-8', errors='surrogatepass'):
-        """text_from_bits:
-        Convert binary into a string
-
-        :param bits: Binary to convert to a string
-        """
-        try:
-            bits = int(bits, 2)
-            return bits.to_bytes((bits.bit_length() + 7) // 8, 'big').decode(encoding,
-                                                                             errors) or '\0'
-        except ValueError:
-            pass
-
-def main():
-    """main: Driver code for the commandline portion of this script
-    """
-    parser = argparse.ArgumentParser(description='Simple Python script to use \
-                                     "Last Bit Stegonography" to hide messages in images')
-
-    parser.add_argument('-e',
-                        '--encode-text',
-                        action="store",
-                        type=str,
-                        help='Text which you want to hide in the specified image')
-
-    parser.add_argument('-d',
-                        '--decode-text',
-                        action="store_true",
-                        help='Attempt to decode hidden text from the specified image')
-
-    parser.add_argument('-f',
-                        '--input-file',
-                        action="store",
-                        type=str,
-                        help='File which you want to operate with. This must be a "png" or a "bmp"')
-
-    parser.add_argument('-o',
-                        '--output-file',
-                        action="store",
-                        type=str,
-                        help='Filename which output will be saved too')
-
-    arguments = parser.parse_args()
-
-    if not arguments.input_file:
-        raise ValueError('You must specify a file using either "-f" or "--file"')
-
-    if arguments.encode_text and arguments.decode_text:
-        raise ValueError("You can't both encode and decode at the same time")
-
-    steg = Steganography(arguments.input_file, arguments.output_file)
-
-    if arguments.encode_text:
-        steg.encode_text(arguments.encode_text)
-
-    if arguments.decode_text:
-        message = steg.decode_text()
-        if not message:
-            print("This image file doesn't contain a message hidden using 'Last Bit Steganography'")
-        else:
-            print(message)
-
-if __name__ == '__main__':
-    import argparse
-    main()
