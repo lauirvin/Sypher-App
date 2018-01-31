7ec10b90d6b5191f3cf5bfa518842f2201f5c959
William Strode <william.strode@gmail.com>
2018-01-31 15:22:22 +0000

    working prototype for encode/decode of RSA

--- py_cryptography/RSAtest.py
+++ py_cryptography/RSAtest.py
@@ -15,13 +15,26 @@ def RSA_encode(message,p,q):
         if d_candidate % e == 0:
             found = True
             d = d_candidate / e
-        
-    return d
 
-print(RSA_encode("hello",3,11))
+    print (d)
+        
+    code = [((i**e) % n) for i in asc2_m]
+    return code
+
+def RSA_decode(code,n):
+    d = int(input('Enter the private key for decoding: '))
+    decode_asc2 = [((i**d) % n) for i in code]
+    message_list = [chr(c) for c in decode_asc2]
+    message = ''.join(message_list)
+    return message
+    
+    
+
+print( RSA_decode((RSA_encode("incredible i can encode things",17,11)),187))
 
 #n = pq
 #z = (p-1)(q-1)
 #e = 1 ~ z with no common factor to z
 #d = ed == 1(mod z)
 #c == m^e (mod n)
+#m == c^d (mod n)
