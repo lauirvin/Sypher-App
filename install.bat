2c4750e5676a2048a5e1afec351aa611db0965e5
James Lee <jamesl33info@gmail.com>
2018-02-04 10:33:05 +0000

    Added install.sh/install.bat to fetch less.js

---
+++ install.bat
@@ -0,0 +1,1 @@
+powershell -Command "Invoke-WebRequest https://raw.githubusercontent.com/less/less.js/3.x/dist/less.js -OutFile gui/less.js"
---
+++ install.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+curl "https://raw.githubusercontent.com/less/less.js/3.x/dist/less.js" --create-dirs --output "gui/less.js"
