20228aba43a9e173390ec84842fcdbf5f27ce4a9
lauirvin <lauirvin98@gmail.com>
2018-02-05 01:12:06 +0000

    Completed upload scene

---
+++ gui/js/browse.js
@@ -0,0 +1,21 @@
+function emptyUpload() {
+  if (document.getElementById("upload").value == "") {
+    document.getElementById("next").style.backgroundColor = "#c0c0c0";
+    document.getElementById("link").href = "";
+  } else {
+    document.getElementById("next").style.backgroundColor = "#535ae8";
+    document.getElementById("link").href = "../index.html";
+  }
+  window.setTimeout("emptyUpload();", 1000 * 0.1);
+}
+
+function previewImage(input) {
+  var reader = new FileReader();
+  reader.onload = function() {
+    var output = document.getElementById("preview");
+    document.getElementById('preview').style.boxShadow = "0px 0px 35px -1px rgba(0,122,255,0.7)";
+    output.src = reader.result;
+    
+  };
+  reader.readAsDataURL(event.target.files[0]);
+}
