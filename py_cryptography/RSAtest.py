7bc19f8e56ca62a2e490ac4551624707ae1b084f
William Strode <william.strode@gmail.com>
2018-02-13 19:44:27 +0000

    RSA cli code with file usage implemented - bug with keygen being constantly called

--- py_cryptography/RSAtest.py
+++ py_cryptography/RSAtest.py
@@ -11,8 +11,17 @@ m == c^d (mod n)
 import math
 import random
 
+def isPrime(num):
+    if num > 1:
+        for i in range(2,num):
+            if (num % i) == 0:
+                return False
+            else:
+                return True
+    return False
+
 def RSA_keygen():
-    primes = [i for i in range (10,50) if isPrime(i)]
+    primes = [i for i in range (10,20) if isPrime(i)]
     p = random.choice(primes)
     primes.remove(p)
     q = random.choice(primes)
@@ -29,24 +38,48 @@ def RSA_keygen():
             found = True
             d = d_candidate / e
 
-    public = []
-    public.append(e)
-    public.append(n)
-    private = []
-    private.append(d)
-    private.append(n)
+    keys = open("keys.txt","w")
+    keys.write(str(n) + '\n')
+    keys.write(str(e) + '\n')
+    keys.write(str(d).strip('.0') + '\n')
 
-    return (public,private)
+    return None
 
-def RSA_encode(message,e,n):
+def RSA_encode(message,keys):
+    keyfile = open(keys,"r")
+    keylines = keyfile.readlines()
+
+    n = int(keylines[0])
+    e = int(keylines[1])
+    keyfile.close()
+    
     asc2_m = [ord(c) for c in message]                
     code = [((i**e) % n) for i in asc2_m]
-    return code
+    code_doc = open("message.txt","w")
+    for i in code:
+        code_doc.write(str(i))
+        code_doc.write("\n")
 
-def RSA_decode(code,d,n):    
-    decode_asc2 = [((i**d) % n) for i in code]
-    message_list = [chr(c) for c in decode_asc2]
-    message = ''.join(message_list)
-    return message
+    code_doc.close()
+    return None
+
+def RSA_decode(message,keys):
+    keyfile = open(keys,"r")
+    keylines = keyfile.readlines()
+
+    n = int(keylines[0])
+    d = int(keylines[2])
+    keyfile.close()
+    
+    code_doc = open(message,"r")
+    stringcode = code_doc.readlines()
+    asc2_code = [int(i) for i in stringcode]
+    plaintext_list = [chr(c) for c in asc2_code]
+    plaintext = ''.join(plaintext_list)
+    code_doc.close()
+    return plaintext
+
+if __name__ == '__main__':
+    RSA_keygen()
 
 
--- py_cryptography/cli_RSA.py
+++ py_cryptography/cli_RSA.py
@@ -4,23 +4,22 @@ from RSAtest import *
 
 def command_line():
     parser = argparse.ArgumentParser(description='Command line interface for RSA encryption')
+    parser.add_argument('-n','--new-keys', action='store', type=bool, help='Whether or not new keys should be generated. MANDATORY.')
     parser.add_argument('-m','--message', action='store', type=str, help='Message to be encoded.')
-    parser.add_argument('-c','--coded-message', action='store', type=list, help='Message to be decoded.')
+    parser.add_argument('-c','--coded-message', action='store', type=str, help='Location of message to be decoded.')    
     arguments = parser.parse_args()
 
-    if not arguments.message and not arguments.coded_message:
-        raise ValueError('No message to encode or decode.')
+    if arguments.new_keys == None:
+        raise ValueError('You must specify whether or not new keys are required.')
 
-    
-    keys = RSA_keygen(arguments.p,arguments.q)
-    print (keys)
+    if arguments.new_keys:
+        RSA_keygen()
 
     if arguments.message:
-        coded = RSA_encode(arguments.message,keys[0][0],keys[0][1])
-        print (coded)
+        coded = RSA_encode(arguments.message,"keys.txt")     
 
     if arguments.coded_message:
-        message = RSA_decode(arguments.coded_message,keys[1][0],keys[1][1])
+        message = RSA_decode(arguments.coded_message,"keys.txt")
         print (message)
                                                                                    
 if __name__ == '__main__':
