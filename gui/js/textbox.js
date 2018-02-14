67cf3806b1930603a6690b95f50bfd56529ce308
lauirvin <lauirvin98@gmail.com>
2018-02-14 22:04:59 +0000

    Optimized textarea for mobile devices

--- gui/js/textbox.js
+++ gui/js/textbox.js
@@ -1,11 +1,19 @@
 function emptyTextbox() {
-    if (document.getElementById("textbox").value == "") {
-        document.getElementById("next").style.backgroundColor = "#c0c0c0" 
-        document.getElementById("link").href = ""
-    }
-    else {
-        document.getElementById("next").style.backgroundColor = "#35B0AB"
-        document.getElementById("link").href = "encode_upload_image.html"
-    }   
-    window.setTimeout("emptyTextbox();", 1000*0.1);
+  if (document.getElementById("textbox").value == "") {
+    document.getElementById("next").style.backgroundColor = "#c0c0c0";
+    document.getElementById("link").href = "";
+  } else {
+    document.getElementById("next").style.backgroundColor = "#35B0AB";
+    document.getElementById("link").href = "encode_upload_image.html";
+  }
+  window.setTimeout("emptyTextbox();", 1000 * 0.1);
+}
+
+function focusTextbox() {
+  var textbox = document.getElementById("textbox");
+  document.getElementById("next").style.display = "none";
+}
+
+function blurTextbox() {
+  document.getElementById("next").style.display = "block";
 }
