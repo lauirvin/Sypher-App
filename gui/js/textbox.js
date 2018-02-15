94c8b308a4a1c4dbfc568ebb939ff7353fd1b8a6
lauirvin <lauirvin98@gmail.com>
2018-02-15 21:18:30 +0000

    Fixed empty links

--- gui/js/textbox.js
+++ gui/js/textbox.js
@@ -5,6 +5,7 @@ function emptyTextbox() {
   } else {
     document.getElementById("next").style.backgroundColor = "#35B0AB";
     document.getElementById("link").href = "encode_upload_image.html";
+    document.getElementById("link").onclick="return true";
   }
   window.setTimeout("emptyTextbox();", 1000 * 0.1);
 }
