bce7e87f920984610c953a913fb2ab4feaed41a6
Gonçalo Bernardo <g.beato@hotmail.com>
2018-02-20 16:09:43 +0000

    Fix minor upload bugs

--- gui/js/browse.js
+++ gui/js/browse.js
@@ -1,5 +1,5 @@
-function encodeEmptyUpload() {
-  if (document.getElementById("upload").value == "") {
+function encodeCheckUpload() {
+  if (document.getElementById("file-name").innerHTML == "") {
     document.getElementById("next").style.backgroundColor = "#c0c0c0";
     document.getElementById("link").href = "";
   } else {
@@ -7,11 +7,11 @@ function encodeEmptyUpload() {
     document.getElementById("link").href = "./encode_download.html";
     document.getElementById("link").onclick="return true";
   }
-  window.setTimeout("encodeEmptyUpload();", 1000 * 0.1);
 }
 
-function decodeEmptyUpload() {
-  if (document.getElementById("upload").value == "") {
+function decodeCheckUpload() {
+  console.log(document.getElementById("preview").hasAttribute("src"))
+  if (!document.getElementById("preview").hasAttribute("src")) {
     document.getElementById("next").style.backgroundColor = "#c0c0c0";
     document.getElementById("link").href = "";
   } else {
@@ -19,7 +19,6 @@ function decodeEmptyUpload() {
     document.getElementById("link").href = "./decode_download.html";
     document.getElementById("link").onclick="return true;";
   }
-  window.setTimeout("decodeEmptyUpload();", 1000 * 0.1);
 }
 
 function previewImage(input) {
@@ -29,6 +28,7 @@ function previewImage(input) {
     document.getElementById("preview").style.background = "none";
     document.getElementById("preview").style.boxShadow = "0px 0px 35px -1px rgba(0,122,255,0.7)";
     output.src = reader.result;
+    decodeCheckUpload();
   };
   reader.readAsDataURL(event.target.files[0]);
 }
