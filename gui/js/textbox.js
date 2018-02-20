108f6473f9759acc90ab3599888adc54cad96969
lauirvin <lauirvin98@gmail.com>
2018-02-20 21:43:43 +0000

    Recorrected page pathing and revamped "Next" button

--- gui/js/textbox.js
+++ gui/js/textbox.js
@@ -1,10 +1,8 @@
 function emptyTextbox() {
   if (document.getElementById("textbox").value == "") {
-    document.getElementById("next").style.backgroundColor = "#c0c0c0";
-    document.getElementById("link").href = "";
+    document.getElementById("link").style.backgroundColor = "#c0c0c0";
   } else {
-    document.getElementById("next").style.backgroundColor = "#35B0AB";
-    document.getElementById("link").href = "encode_upload_image.html";
+    document.getElementById("link").style.backgroundColor = "#35B0AB";
     document.getElementById("link").onclick = "return true";
   }
   window.setTimeout("emptyTextbox();", 1000 * 0.1);
@@ -12,10 +10,10 @@ function emptyTextbox() {
 
 function emptyInputbox() {
   if (document.getElementById("key1" && "key2").value == "") {
-    document.getElementById("next").style.backgroundColor = "#c0c0c0";
+    document.getElementById("link").style.backgroundColor = "#c0c0c0";
   } else {
-    document.getElementById("next").style.backgroundColor = "#35B0AB";
-    document.getElementById("next").onclick = "return true";
+    document.getElementById("link").style.backgroundColor = "#35B0AB";
+    document.getElementById("link").onclick = "return true";
   }
   window.setTimeout("emptyInputbox();", 1000 * 0.1);
 }
