01fef43a25bd1b5854bc4eb9707ddbf446bcf6d9
James Lee <jamesl33info@gmail.com>
2018-02-23 23:36:25 +0000

    Changed to use path.join(). Also fixed nasty logic error.
    Logic error:
    If the image file uploaded contained a copy of itself it would result in the
    image being prematurely deleted.

--- server/index.js
+++ server/index.js
@@ -3,13 +3,14 @@
 'use strict';
 
 const exec = require('child_process').exec;
+const express = require('express');
+const fileUpload = require('express-fileupload');
 const fs = require('fs');
+const path = require('path');
 const rimraf = require('rimraf');
 
-const express = require('express');
 const app = express();
 
-const fileUpload = require('express-fileupload');
 app.use(fileUpload());
 
 function genRandom (low, high, length) {
@@ -21,17 +22,17 @@ function genRandom (low, high, length) {
 }
 
 app.get('/encode', (req, res) => {
-    res.sendFile(__dirname + '/encode.html');
+    res.sendFile(path.join(__dirname, '/encode.html'));
 });
 
 app.get('/decode', (req, res) => {
-    res.sendFile(__dirname + '/decode.html');
+    res.sendFile(path.join(__dirname, '/decode.html'));
 });
 
 app.post('/encode', (req, res) => {
-    const enocdeDir = `/tmp/steg-encode-${genRandom(0, 9, 8)}/`;
+    const encodeDir = path.join('/tmp/steg-encode-') + genRandom(0, 9, 8);
 
-    fs.mkdir(enocdeDir, (error) => {
+    fs.mkdir(encodeDir, (error) => {
         if (error) {
             throw error;
         }
@@ -44,19 +45,19 @@ app.post('/encode', (req, res) => {
         return res.status(500).send('Failed to retrieve files');
     }
 
-    image.mv(enocdeDir + image.name, (error) => {
+    image.mv(path.join(encodeDir, image.name), (error) => {
         if (error) {
             return res.status(500).send(error);
         }
     });
 
-    file.mv(enocdeDir + file.name, (error) => {
+    file.mv(path.join(encodeDir, file.name), (error) => {
         if (error) {
             return res.status(500).send(error);
         }
     });
 
-    exec(`../steganography/bin/steganography -i ${enocdeDir}${image.name} -e ${enocdeDir}${file.name}`, (error, stdout, stderr) => {
+    exec(`../steganography/bin/steganography -i ${path.join(encodeDir, image.name)} -e ${path.join(encodeDir, file.name)}`, (error, stdout, stderr) => {
         if (error) {
             if (stderr.trim() === 'Error: Failed to store file in image the image is too small') {
                 res.status(500).send(stderr.trim());
@@ -66,14 +67,14 @@ app.post('/encode', (req, res) => {
                 console.log(stdout).trim();
             }
 
-            res.download(enocdeDir + 'steg-' + image.name.substr(0, image.name.lastIndexOf('.')) + '.png', (error) => {
+            res.download(path.join(encodeDir, 'steg-' + image.name.substr(0, image.name.lastIndexOf('.'))) + '.png', (error) => {
                 if (error) {
                     console.log(error);
                 }
             });
         }
 
-        rimraf(enocdeDir, (error) => {
+        rimraf(encodeDir, (error) => {
             if (error) {
                 throw error;
             }
@@ -82,7 +83,7 @@ app.post('/encode', (req, res) => {
 });
 
 app.post('/decode', (req, res) => {
-    const decodeDir = `/tmp/steg-decode-${genRandom(0, 9, 8)}/`;
+    const decodeDir = path.join('/tmp/steg-decode-') + genRandom(0, 9, 8);
 
     fs.mkdir(decodeDir, (error) => {
         if (error) {
@@ -96,13 +97,13 @@ app.post('/decode', (req, res) => {
         return res.status(500).send('Failed to retrieve files');
     }
 
-    image.mv(decodeDir + image.name, (error) => {
+    image.mv(path.join(decodeDir, image.name), (error) => {
         if (error) {
             return res.status(500).send(error);
         }
     });
 
-    exec(`../steganography/bin/steganography -i ${decodeDir}${image.name} -d`, (error, stdout, stderr) => {
+    exec(`../steganography/bin/steganography -i ${path.join(decodeDir, image.name)} -d`, (error, stdout, stderr) => {
         if (error) {
             if (stderr.trim() === 'Error: This image does not appear to contain a hidden file') {
                 res.status(500).send(stderr.trim());
@@ -112,9 +113,11 @@ app.post('/decode', (req, res) => {
                 console.log(stdout).trim();
             }
 
-            fs.unlinkSync(decodeDir + image.name);
+            if (fs.readdirSync(decodeDir).length !== 1) {
+                fs.unlinkSync(path.join(decodeDir, image.name));
+            }
 
-            res.download(decodeDir + fs.readdirSync(decodeDir)[0], (error) => {
+            res.download(path.join(decodeDir, fs.readdirSync(decodeDir)[0]), (error) => {
                 if (error) {
                     console.log(error);
                 }
