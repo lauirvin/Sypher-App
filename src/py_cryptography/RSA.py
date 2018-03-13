0a8ab6b38aa7daee0182e6d0cec7a56b040911ac
William Strode <william.strode@gmail.com>
2018-03-13 15:19:27 +0000

    OO and filepath patch applied to RSA code

--- src/py_cryptography/RSA.py
+++ src/py_cryptography/RSA.py
@@ -1,3 +1,5 @@
+#!/usr/bin/python3
+
 '''
 Variable derivations:
 n = pq
@@ -15,129 +17,134 @@ import unittest
 import time
 import os
 
+
 class RSA:
-        def __init__(self,outputDir = os.path.dirname(os.path.realpath(__file__))):
-                self.outputDir = outputDir
+    def __init__(self, outputDir=None):
+        if outputDir is None:
+            outputDir = os.path.dirname(os.path.realpath(__file__))
 
-        def isPrime(self,num):
-                '''Simple function to ascertain whether a given integer is a prime number.
-                returns True if the number is prime, and False otherwise.'''
-                if num == 2:
-                        return True
-                if num < 2 or num % 2 == 0:
-                        return False
-                for n in range(3,int(num**0.5)+2,2):
-                                if (num % n) == 0:
-                                        return False            
+        self.outputDir = outputDir
+
+    def isPrime(self, num):
+        '''Simple function to ascertain whether a given integer is a prime number.
+        returns True if the number is prime, and False otherwise.'''
+        if num == 2:
                 return True
+        if num < 2 or num % 2 == 0:
+                return False
+        for n in range(3, int(num**0.5) + 2, 2):
+                        if (num % n) == 0:
+                                return False
+        return True
 
-        def egcd(self,a, b):
-                '''Recursive implementation of Euclid's extended algorithm. Expects two integers
-                as input and returns a triple (g,x,y) such that ax + by = g = gcd(a,b)'''
-                if a == 0:
-                        return (b,0,1)
-                else:
-                        g, x, y = egcd(b % a, a)
-                        return (g, y - (b // a) * x, x)
+    def egcd(self, a, b):
+        '''Recursive implementation of Euclid's extended algorithm. Expects two integers
+        as input and returns a triple (g,x,y) such that ax + by = g = gcd(a,b)'''
+        if a == 0:
+                return (b, 0, 1)
+        else:
+                g, x, y = self.egcd(b % a, a)
+                return (g, y - (b // a) * x, x)
 
-        def mul_inv(self,e, n):
-                '''Makes use of the egcd function to recursively find the modular inverse of e. '''
-                g, x, _ = egcd(e,n)
-                if g == 1:
-                        return x % n
-                
-        def RSA_keygen(self):
-                '''Function for generating a new set of RSA keys. Generates two large prime numbers
-                and calls mul_inv to generate the decryption key d. Output is stored as pickled tuples
-                pubkey and privkey respectively for the public and private components of the key.'''
-                primes = [i for i in range (9900000,10000000) if isPrime(i)]
-                p = random.SystemRandom(primes)
-                primes.remove(p)
-                q = random.SystemRandom(primes)
-                n = p * q    
-                e = 65537
-                #phi is euler's totient - the count of positive integers up to n that are coprime to n.
-                phi = (p-1)*(q-1)   
-                #lambda is derived from phi as phi / gcd((p-1)(q-1))
-                lam = int(phi / math.gcd((p-1),(q-1)))     
-                d = mul_inv(e,lam)
+    def mul_inv(self, e, n):
+        '''Makes use of the egcd function to recursively find the modular inverse of e. '''
+        g, x, _ = self.egcd(e, n)
+        if g == 1:
+                return x % n
 
-                pubkey = (n,e)
-                privkey = (n,d)    
+    def RSA_keygen(self):
+        '''Function for generating a new set of RSA keys. Generates two large prime numbers
+        and calls mul_inv to generate the decryption key d. Output is stored as pickled tuples
+        pubkey and privkey respectively for the public and private components of the key.'''
+        primes = [i for i in range(9900000, 10000000) if self.isPrime(i)]
 
-                with open(self.outputDir + "/public.b","wb") as f:
-                        pickle.dump(pubkey,f)
+        try:
+            p = random.SystemRandom(primes)
+        except TypeError:
+            p = random.choice(primes)
 
-                with open(self.outputDir + "/private.b","wb") as f:
-                        pickle.dump(privkey,f)
+        primes.remove(p)
 
-                return None
+        try:
+            q = random.SystemRandom(primes)
+        except TypeError:
+            q = random.choice(primes)
 
-        def RSA_encode(self,message,keys):
-                '''Encodes a message using RSA public key cryptography. Output is stored as a pickled list in the
-                file message.b for later usage.'''
-                with open(keys,"rb") as f:
-                        key_tup = pickle.load(f)
+        n = p * q
+        e = 65537
+        # phi is euler's totient - the count of positive integers up to n that are coprime to n.
+        phi = (p-1)*(q-1)
+        # lambda is derived from phi as phi / gcd((p-1)(q-1))
+        lam = int(phi / math.gcd((p-1), (q-1)))
+        d = self.mul_inv(e, lam)
 
-                n,e = key_tup
-                                                
-                code = [pow(ord(i),e,n) for i in message]
-                with open("message.b","wb") as f:
-                        pickle.dump(code,f)
-                
-                return None
+        pubkey = (n, e)
+        privkey = (n, d)
 
-        def RSA_decode(self,message,keys):
-                '''Decodes a message encrypted using RSA. Output is returned as a string containing the plaintext
-                message.'''
-                with open(keys,"rb") as f:
-                        key_tup = pickle.load(f)
+        with open(self.outputDir + "/public.b", "wb") as f:
+                pickle.dump(pubkey, f)
 
-                n,d = key_tup  
+        with open(self.outputDir + "/private.b", "wb") as f:
+                pickle.dump(privkey, f)
 
-                with open(message,"rb") as f:
-                        code = pickle.load(f)  
-                
-                plaintext_list = [chr(pow(i,d,n)) for i in code]
-                plaintext = ''.join(plaintext_list)    
-                return plaintext
+        return None
 
-        def parse_txt(self,file):
-                with open(file,"r") as f:
-                        string = f.read()
+    def RSA_encode(self, message, keyLocation):
+        '''Encodes a message using RSA public key cryptography. Output is stored as a pickled list in the
+        file message.b for later usage.'''
+        with open(keyLocation, "rb") as f:
+                key_tup = pickle.load(f)
 
-                return string
+        n, e = key_tup
+
+        code = [pow(ord(i), e, n) for i in message]
+        with open("message.b", "wb") as f:
+                pickle.dump(code, f)
+
+        return None
+
+    def RSA_decode(self, message, keyLocation):
+        '''Decodes a message encrypted using RSA. Output is returned as a string containing the plaintext
+        message.'''
+        with open(keyLocation, "rb") as f:
+                key_tup = pickle.load(f)
+
+        n, d = key_tup
+
+        with open(message, "rb") as f:
+                code = pickle.load(f)
+
+        plaintext_list = [chr(pow(i, d, n)) for i in code]
+        plaintext = ''.join(plaintext_list)
+        return plaintext
+
+    def parse_txt(self, file):
+        with open(file, "r") as f:
+                string = f.read()
+
+        return string
+
 
 class RSA_tests(unittest.TestCase):
+    def setUp(self):
+        self.rsa = RSA()
+        self._started_at = time.time()
 
-	def setUp(self):
-		self._started_at = time.time()
+    def tearDown(self):
+        elapsed = time.time() - self._started_at
+        print('{} ({}s'.format(self.id(), elapsed))
 
-	def tearDown(self):
-		elapsed = time.time() - self._started_at
-		print ('{} ({}s'.format(self.id(), elapsed))
+    def test_1(self):
+        self.rsa.RSA_keygen()
 
-	def test_1(self):
-		RSA_keygen()
+    def test_2(self):
+        print('Essay encode test running... ')
+        self.rsa.RSA_encode(self.rsa.parse_txt('test_essay.txt'), 'public.b')
 
-	def test_2(self):
-		print('Essay encode test running... ')
-		RSA_encode(parse_txt('test_essay.txt'),'public.b')
-		
-	def test_3(self):
-		print('Essay decode test running... ')
-		RSA_decode('message.b','private.b')
+    def test_3(self):
+        print('Essay decode test running... ')
+        self.rsa.RSA_decode('message.b', 'private.b')
 
-#Shakespeare test code commented - requires utf-8 encoding in parse_txt
-'''	
-	def test_4(self):
-                print('Shakespeare test running... ')
-		RSA_encode(parse_txt('shakespeare.txt'),'public.b')
 
-	def test_5(self):
-		print('Shakespeare decode test running... ')
-		RSA_decode('message.b','private.b')
-'''
 if __name__ == '__main__':
-	unittest.main()
-	
+    unittest.main()
